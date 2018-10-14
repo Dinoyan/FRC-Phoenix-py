bf1355f820f403feb9288a834569ebac1e1b4bf6
Dinoyan <dinoyan.g@hotmail.com>
2018-10-13 22:26:37 -0400

    auto handler

---
+++ Autonomous_mode_handler.py
@@ -0,0 +1,10 @@
+class AutonomousModeHandler():
+
+	def __init__(self, drivetrain, sensors, elevator, intake):
+		self.sensors = sensors
+		self.drivetrain = drivetrain
+		self.elevator = elevator
+		self.intake = intake
+
+	def AutoModeSelect(position):
+		pass
