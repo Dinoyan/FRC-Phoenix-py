0174be8c2a5910d39d6539773e3388188b202f36
Dinoyan <dinoyan.g@hotmail.com>
2018-10-16 16:41:36 -0400

    formatted the code

--- Autonomous_mode_handler.py
+++ Autonomous_mode_handler.py
@@ -1,34 +1,35 @@
 import robot_constant
 
+
 class AutonomousModeHandler():
 
-	def __init__(self, drivetrain, sensors, elevator, intake):
-		self.sensors = sensors
-		self.drivetrain = drivetrain
-		self.elevator = elevator
-		self.intake = intake
+    def __init__(self, drivetrain, sensors, elevator, intake):
+        self.sensors = sensors
+        self.drivetrain = drivetrain
+        self.elevator = elevator
+        self.intake = intake
 
-	def AutoModeSelect(self, position, gameData):
-		
-		if (position == robot_constant.LEFT_POS):
+    def AutoModeSelect(self, position, gameData):
 
-			if (gameData[1] == 'L'):
-				pass
+        if (position == robot_constant.LEFT_POS):
 
-			else:
-				pass
+            if (gameData[1] == 'L'):
+                pass
 
-		elif(position == robot_constant.RIGHT_POS):
-			if (gameData[1] == 'R'):
-				pass
-			else:
-				pass
+            else:
+                pass
 
-		elif(position == robot_constant.LEFT_POS):
-			if (gameData[0] == 'L'):
-				pass
-			else:
-				pass
+        elif(position == robot_constant.RIGHT_POS):
+            if (gameData[1] == 'R'):
+                pass
+            else:
+                pass
 
-		else:
-			pass
+        elif(position == robot_constant.LEFT_POS):
+            if (gameData[0] == 'L'):
+                pass
+            else:
+                pass
+
+        else:
+            pass
--- Joystick_handler.py
+++ Joystick_handler.py
@@ -4,17 +4,12 @@ import robot_map
 
 class JoystickHandler():
 
+    def __init__(self):
+        self.driveStick = wpilib.Joystick(robot_map.DRIVE_STICK)
+        self.cubeStick = wpilib.Joystick(robot_map.CUBE_STICK)
 
-	def __init__(self):
-		self.driveStick = wpilib.Joystick(robot_map.DRIVE_STICK)
-		self.cubeStick = wpilib.Joystick(robot_map.CUBE_STICK)
+    def getDriveStick(self):
+        return self.driveStick
 
-	def getDriveStick(self):
-		return self.driveStick
-
-	def getCubeStick(self):
-		return self.cubeStick
-
-	
-
-
+    def getCubeStick(self):
+        return self.cubeStick
